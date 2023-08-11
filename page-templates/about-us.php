29e611ea3559d4e90032c4621a8661bc6b075f70
Goncalves Théau <theau26@gmail.com>
2023-08-11 09:30:00 +0200

    Double query

--- page-templates/about-us.php
+++ page-templates/about-us.php
@@ -1,11 +1,10 @@
 <?php
 /* Template Name: About us */
 
-$args    = [
-	'post_type'      => 'team_member',
-	'posts_per_page' => 8
+$querySections = [
+	'formateur' => 'Formateurs',
+	'apprenant' => 'Apprenants'
 ];
-$members = new WP_Query( $args );
 
 
 get_header();
@@ -25,34 +24,57 @@ get_header();
                     </div>
                 </div>
 
-                <div class="row">
-					<?php if ( $members->have_posts() ) : ?>
-                        <!-- pagination here -->
+				<?php foreach ( $querySections as $term => $title ):
+					$args = [
+						'post_type'      => 'team_member',
+//						'posts_per_page' => -1,
+						'tax_query'      => [
+							[
+								'taxonomy' => 'member_type',
+								'field'    => 'slug',
+								'terms'    => $term,
+							]
+						],
+					];
+					$members = new WP_Query( $args );
 
-                        <!-- the loop -->
-						<?php
-						while ( $members->have_posts() ) :
-							$members->the_post();
+
+					?>
+
+                    <div class="row">
+						<?php if ( $members->have_posts() ) : ?>
+                            <!-- pagination here -->
+                        <div class="col-12">
+                            <h2><?php echo $title;?></h2>
+                        </div>
+                            <!-- the loop -->
+							<?php
+							while ( $members->have_posts() ) :
+								$members->the_post();
+								?>
+                                <div class="col-lg-3 col-md-4 col-sm-6">
+									<?php get_template_part( 'partials/card/card', 'member' ); ?>
+                                </div>
+
+								<?php
+								wp_reset_postdata();
+							    endwhile;
 							?>
-                            <div class="col-lg-3 col-md-4 col-sm-6">
-                                <?php get_template_part('partials/card/card', 'member');?>
-                            </div>
+                            <!-- end of the loop -->
 
-						<?php endwhile; ?>
-                        <!-- end of the loop -->
+							<?php wp_reset_postdata(); ?>
 
-                        <!-- pagination here -->
+						<?php else : ?>
+                            <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+						<?php endif; ?>
 
-						<?php wp_reset_postdata(); ?>
-
-					<?php else : ?>
-                        <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-					<?php endif; ?>
-
-                </div>
+                    </div>
+				<?php endforeach; ?>
             </div>
         </main>
     </div>
 
 <?php
+
+
 get_footer();
